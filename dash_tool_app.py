309b3a90d26935b62f924cf823f854edaf8f725a
kkrusere <kuzirusere@gmail.com>
2024-03-30 16:13:20 -0400

    update

--- dash_tool_app.py
+++ dash_tool_app.py
@@ -16,8 +16,11 @@ with col1:
 
 with col2:
     st.markdown(" <h1 style='text-align: center;'>YouTube Comment Analyser</h1>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center;'><i><b> </b></i></p>", unsafe_allow_html=True)
-    st.markdown("<center><img src='https://github.com/kkrusere/Market-Basket-Analysis-on-the-Online-Retail-Data/blob/main/Assets/MBA.jpg?raw=1' width=600/></center>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center;'><i><b> YouTube-comments-Analyzer is a tool to analyze audience sentiment, "
+                "perspective, topics, and engagement of YouTube video comments. It collects and preprocesses comments, performs "
+                "sentiment analysis and topic modeling, and visualizes findings through an interactive dashboard. Beneficial for "
+                "content creators, marketers, and researchers. </b></i></p>", unsafe_allow_html=True)
+    st.markdown("<center><img src='https://github.com/kkrusere/youTube-comments-Analyzer/blob/main/assets/comments_analyzer.jpg?raw=1' width=600/></center>", unsafe_allow_html=True)
 
 
 with col3:
