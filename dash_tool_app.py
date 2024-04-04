40a56e7c4408fbf3e01d2ca834490793de9895a6
kkrusere <kuzirusere@gmail.com>
2024-04-04 15:08:19 -0400

    update

--- dash_tool_app.py
+++ dash_tool_app.py
@@ -131,6 +131,11 @@ with col3:
 #######################################################**************getting some comments**************##################################################
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 @st.experimental_singleton
 def installff():
@@ -144,11 +149,30 @@ opts.add_argument("--headless")
 driver = webdriver.Firefox(options=opts)
 
 
-driver.get('http://example.com')
-st.write(driver.page_source)
+##########################################################   https://www.youtube.com/watch?v=ybl8IvhGAJ4
 
+data = []
 
+try:
+    wait = WebDriverWait(driver, 1)
 
+    # Open the YouTube video
+    driver.get("https://www.youtube.com/watch?v=ybl8IvhGAJ4")
+
+    # Scroll down to load comments
+    for item in range(200):
+        wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
+        time.sleep(1)
+
+    # Get all comments
+    for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content"))):
+        data.append(comment.text)
+
+finally:
+    # Close the WebDriver session
+    driver.quit()
+
+st.write(data[0])
 
 ###########################################################################################################################################################
 st.markdown("----")
